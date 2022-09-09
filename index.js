97780f939d83119ccb9c2a5300f3599e8dea6993
Raph3046 <Raphael3046@gmail.com>
2022-09-09 20:58:54 +0100

    javascript functions

---
+++ index.js
@@ -0,0 +1,28 @@
+// function twins (){
+    // console.log('Daniel and Raphael')
+// }
+// twins() 
+
+// function sum (){
+//     let x = 1
+//     let y = 2
+//     return x + y
+// }
+// console.log(sum())
+
+// function fullName(firstName, lastName){
+//     return firstName + lastName
+// }
+// let x = fullName("Raphael ", "Awoyale")
+// console.log(x)
+// // console.log(fullName("Raphael ", "Awoyale"))
+
+// function sum (sum1, sum2){
+//     return sum1 + sum2
+// }
+// console.log(sum (10, 6))
+
+function sum (sum1, sum2){
+    return sum1 * sum2 * 0.5
+}
+console.log( sum (6, 10))
